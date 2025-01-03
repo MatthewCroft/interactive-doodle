329f3f7d3a3511042f1630ff4b6dd6ae41c27df1
Matthew Croft <matthewpcroft1@gmail.com>
2025-01-03 13:03:35 -0500

    updating server to return correct url for localhost and heroku

--- src/main/java/com/startup/game/controllers/StompConfigController.java
+++ src/main/java/com/startup/game/controllers/StompConfigController.java
@@ -1,17 +1,17 @@
 package com.startup.game.controllers;
 
 import com.startup.game.models.StompBrokerConfig;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StompConfigController {
-    @Value("${stomp.broker-url}")
-    private String brokerUrl;
-
     @GetMapping("/config/stomp")
-    public StompBrokerConfig getBrokerUrl() {
+    public StompBrokerConfig getBrokerUrl(HttpServletRequest request) {
+        String serverPort = request.getServerPort() == 80 || request.getServerPort() == 443 ? "" : String.valueOf(request.getServerPort());
+        String brokerUrl = String.format("ws://%s:%s/game-websocket", request.getServerName(), serverPort);
         return new StompBrokerConfig(brokerUrl);
     }
 }
